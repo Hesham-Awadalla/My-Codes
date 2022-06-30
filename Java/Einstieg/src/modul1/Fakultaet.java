f1f51ec7e3840f86a71633d1bb31cc32ff9b4802
Hesham Awadalla <92855596+Hesham-Awadalla@users.noreply.github.com>
2022-06-30 20:28:45 +0200

    update

--- Java/Einstieg/src/modul1/Fakultaet.java
+++ Java/Einstieg/src/modul1/Fakultaet.java
@@ -1,15 +1,18 @@
 package modul1;
 import java.util.Scanner;
 
-public class Fakultaet { // Aufgabe 3
+public class Fakultaet
+{ // Aufgabe 3
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 
 		System.out.println("Geben Sie bitte eine Zahl >1 ein: (Bitte beachten Sie dass 0! und 1! beide per Definition gleich 1 sind)");
 		Scanner sc = new Scanner(System.in);
 		int Zahl = sc.nextInt();
 		long F = 1L;
-		for (int i = 0; i < Zahl; i++) {
+		for (int i = 0; i < Zahl; i++)
+		{
 			F = F * (i + 1);
 		}
 		System.out.println(Zahl + "!= " + F);
